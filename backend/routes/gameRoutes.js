5343dccbbaed31a194f6036bc7bc6ccbfe623b6e
AMAAN SARFARAZ <sarfarazmohammad369@gmail.com>
2025-05-07 12:24:32 +0530

    removed extra file

--- backend/routes/gameRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const { createGame, getGame } = require("../controllers/gameController");
-
-const router = express.Router();
-
-router.post("/create", createGame);
-router.get("/:roomId", getGame);
-
-module.exports = router;
