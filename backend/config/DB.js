a5218833bc4e184362619b587caa04c0892b89da
AmaanSarfaraz <sarfarazmohammad369@gmail.com>
2025-05-11 14:35:39 +0530

    some minor changes

--- backend/config/DB.js
+++ backend/config/DB.js
@@ -1,10 +1,9 @@
 import mongoose from "mongoose";
 export const connectDB = async () => {
-  const connectionInstance = await mongoose
-    .connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    })
-    .then(() => console.log("MongoDB connected"))
-    .catch((err) => console.error(err));
+  try {
+    const connectionInstance = await mongoose.connect(process.env.MONGO_URI);
+    console.log("MongoDB connected");
+  } catch (error) {
+    console.error(`error connecting database, error: ${error.message}`);
+  }
 };
